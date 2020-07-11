71ff0ceeaca2cc6c265aed154449d83a41485d8c
Noodles <jacek.kloska@gmail.com>
2020-07-11 08:22:13 +0200

    Dodano Pole MothersName

--- WebApiStudents/Models/Student.cs
+++ WebApiStudents/Models/Student.cs
@@ -18,6 +18,8 @@ namespace WebApiStudents.Models
 
         public int Album { get; set; }
 
+        public int MothersName { get; set; }
+
         public DateTime BirthDate { get; set; }
 
         /// <summary>
